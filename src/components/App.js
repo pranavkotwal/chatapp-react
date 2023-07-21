7d21a1425f8cb48d805747a3d9c9f5f07cd2b832
pranavkotwal <pkotwal24@gmail.com>
2023-07-21 23:41:34 +0530

    build troubleshoot

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Chat from '../components/Chat'
+
 
 const App = () => {
   return (
